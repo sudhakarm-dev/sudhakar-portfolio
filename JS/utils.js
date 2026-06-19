a3f9b7bb53003e6b17221580d203245c8925f9d8
sudhakarm-dev <msudhakar.dev@gmail.com>
2026-06-19 21:00:50 +0530

    design change updated

--- JS/utils.js
+++ JS/utils.js
@@ -1,27 +1,5 @@
 const menu = document.getElementById("menu");
 const nav = document.getElementById("nav");
-const bar1 = document.getElementById("bar1");
-const bar2 = document.getElementById("bar2");
-const bar3 = document.getElementById("bar3");
-
-// To change the navlinks size by checking the screen size and changing the position to left
-let x=0;
-menu.onclick = ()=>{
-    if(x === 0){
-        nav.classList.toggle("active");
-        bar1.style.display = "none";
-        bar2.style.transform = "rotate(50deg)";
-        bar3.style.transform = "translateY(-12px) rotate(310deg)";
-        x=1;
-    }
-    else{
-        nav.classList.toggle("active");
-        bar1.style.display = "block";
-        bar2.style.transform = "rotate(0deg)";
-        bar3.style.transform = "translateY(0px) rotate(0deg)";
-        x=0;
-    }
-}
 
 // this function is used for the navbar to hide when scroll down
 const navbar = document.getElementById("navbar"); 
@@ -33,13 +11,19 @@ window.addEventListener('wheel', (event) => {
         }
 
     } else if (event.deltaY < 0) {
-        navbar.style.top = "10px";
+        navbar.style.top = "0px";
         bar1.style.display = "block";
         bar2.style.transform = "rotate(0deg)";
         bar3.style.transform = "translateY(0px) rotate(0deg)";
         // console.log('Scrolling up');
     }
 });
+
+// to close Nav bar
+const closeBtn = document.querySelector(".closeBtn");
+closeBtn.onclick = ()=>{
+    nav.style.display = "none";
+}
 
 // to maximize and minimize the experience section card's
 const downAngle = document.querySelectorAll(".fa-angle-down");
