b1f8b1ad9a79653a5b17e5f28e3ef4bd1c7a7bfb
sudhakarm-dev <msudhakar.dev@gmail.com>
2026-07-06 11:14:40 +0530

    no changes

--- JS/utils.js
+++ JS/utils.js
@@ -1,34 +1,6 @@
-const menu = document.getElementById("menu");
-const nav = document.getElementById("nav");
+// experience box height auto and fit content
+const downArow = document.querySelector(".fa-angle-down");
 
-// this function is used for the navbar to hide when scroll down
-const navbar = document.getElementById("navbar"); 
-window.addEventListener('wheel', (event) => {
-    if (event.deltaY > 0) {
-        navbar.style.top = "-80px";
-        if (window.innerWidth < 768) {
-            nav.classList.remove("active");
-        }
-
-    } else if (event.deltaY < 0) {
-        navbar.style.top = "0px";
-        bar1.style.display = "block";
-        bar2.style.transform = "rotate(0deg)";
-        bar3.style.transform = "translateY(0px) rotate(0deg)";
-        // console.log('Scrolling up');
-    }
-});
-
-// to close Nav bar
-const closeBtn = document.querySelector(".closeBtn");
-closeBtn.onclick = ()=>{
-    nav.style.display = "none";
-}
-
-// to maximize and minimize the experience section card's
-const downAngle = document.querySelectorAll(".fa-angle-down");
-downAngle.forEach(angle => {
-    angle.addEventListener("click", () => {
-        angle.closest(".card").classList.toggle("active");
-    });
-});
+downArow.addEventListener('click',()=>{
+    
+})
